4b6268fc8d354e493933238b0a34c2e2a0ff9de7
minjun <hitobi1014@gmail.com>
2024-12-11 23:58:26 +0900

    chore. jpa, querydsl, db 의존 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.0"
+	kotlin("jvm") version "1.9.22"
+	kotlin("kapt") version "1.9.22"
+	kotlin("plugin.jpa") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+	kotlin("plugin.allopen") version "1.9.22"
+
+	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com"
@@ -20,14 +23,33 @@ repositories {
 }
 
 dependencies {
+	// Spring
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// Database
+	runtimeOnly("org.postgresql:postgresql")
+
+	// Querydsl
+	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
+	// Development Tools
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+	// Logging
+	implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
+
+	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
+	jvmToolchain(17)
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
@@ -37,6 +59,10 @@ allOpen {
 	annotation("jakarta.persistence.Entity")
 	annotation("jakarta.persistence.MappedSuperclass")
 	annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+	annotation("jakarta.persistence.Entity")
 }
 
 tasks.withType<Test> {
