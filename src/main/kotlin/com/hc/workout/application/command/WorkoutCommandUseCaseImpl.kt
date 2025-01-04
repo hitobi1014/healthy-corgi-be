902f38580adbacd45a262a163d5210cc5e1ecb88
minjun <hitobi1014@gmail.com>
2025-01-04 23:08:08 +0900

    feat. 임시 회원 정보 가져오기

--- src/main/kotlin/com/hc/workout/application/command/WorkoutCommandUseCaseImpl.kt
+++ src/main/kotlin/com/hc/workout/application/command/WorkoutCommandUseCaseImpl.kt
@@ -2,6 +2,7 @@ package com.hc.workout.application.command
 
 import com.hc.member.infrastructure.entity.MemberEntity
 import com.hc.member.infrastructure.repository.command.MemberCommandRepository
+import com.hc.shared.security.MemberContext
 import com.hc.workout.application.PhotoUseCase
 import com.hc.workout.domain.arregates.PhotoUploadInfo
 import com.hc.workout.domain.arregates.WorkoutVerification
@@ -15,7 +16,7 @@ import org.springframework.stereotype.Service
 class WorkoutCommandUseCaseImpl(
     private val photoUseCase: PhotoUseCase,
     private val workoutCommandRepository: WorkoutCommandRepository,
-    private val memberCommandRepository: MemberCommandRepository, // 나중에 회원 정보 가져오는 메소드로 변경하기
+    private val memberContext: MemberContext,
 ) : WorkoutCommandUseCase {
 
     override fun saveWorkoutVerification(dto: SaveWorkoutRequest) {
@@ -35,7 +36,7 @@ class WorkoutCommandUseCaseImpl(
 
         // step04. 운동 인증 저장
         // TODO 회원 정보 가져오는 메소드로 변경하기
-        val member: MemberEntity = memberCommandRepository.findById(1).get() // 임시
+        val member: MemberEntity = memberContext.getPrincipal()
         val workoutVerificationEntity =
             WorkoutVerification.addWorkoutVerification(dto, photoInfoList).toEntity(member)
         workoutCommandRepository.save(workoutVerificationEntity)
